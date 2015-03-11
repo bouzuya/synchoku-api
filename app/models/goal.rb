d996219da2c096ddb1f505e3206c6c4db8596f38
bouzuya <m@bouzuya.net>
2015-03-11 23:02:42 +0900

    add simple validation for Goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -12,4 +12,13 @@
 #
 
 class Goal < ActiveRecord::Base
+  validates :date, presence: true
+  validates :token, presence: true
+  validates :value, presence: true,
+                    numericality: {
+                      only_integer: true,
+                      greater_than_or_equal_to: 0,
+                      less_than_or_equal_to: 1000000000
+                    }
+  validates :visible, presence: true
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -13,6 +13,26 @@
 
 require 'rails_helper'
 
-RSpec.describe Goal, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Goal, type: :model do
+  describe '#date' do
+    it { is_expected.to validate_presence_of(:date) }
+  end
+
+  describe '#token' do
+    it { is_expected.to validate_presence_of(:token) }
+  end
+
+  describe '#value' do
+    it { is_expected.to validate_presence_of(:value) }
+    it do
+      is_expected.to validate_numericality_of(:value)
+        .only_integer
+        .is_greater_than_or_equal_to(0)
+        .is_less_than_or_equal_to(1000000000)
+    end
+  end
+
+  describe '#visible' do
+    it { is_expected.to validate_presence_of(:visible) }
+  end
 end
