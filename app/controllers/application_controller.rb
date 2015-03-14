da444e4bd8abd523ca5dac5831b2d9a9ac35d4c0
bouzuya <m@bouzuya.net>
2015-03-14 18:57:34 +0900

    fix request format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_format
 
   def set_format
-    render json: { status: 'error' }, status: :not_acceptable \
-      unless request.format == 'json'
+    request.format = 'json'
   end
 end
