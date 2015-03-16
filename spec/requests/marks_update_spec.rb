a81bdb6e639f031bf0427846fc8ac67d65234a00
bouzuya <m@bouzuya.net>
2015-03-16 23:03:45 +0900

    fix marks#update spec

--- spec/requests/marks_update_spec.rb
+++ spec/requests/marks_update_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'marks#update', type: :request do
   let!(:goal) { create(:goal) }
   let!(:model) { create(:mark, goal: goal) }
 
-  let!(:env) { nil }
   let!(:params) do
     {
       id: model.id,
@@ -16,15 +15,27 @@ RSpec.describe 'marks#update', type: :request do
   let!(:method) { :patch }
   let!(:path) { mark_path(model) }
 
-  before { send(method, path, params, env) }
-  subject { response }
-  its(:status) { is_expected.to be 200 }
-  its(:body) do
-    is_expected.to match_json_expression(
-      id: params[:id],
-      goal_id: params[:goal_id],
-      date: params[:date].iso8601,
-      value: params[:value]
-    )
+  context 'without authorization header' do
+    let!(:env) { nil }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 401 }
+    its(:body) { is_expected.to match_json_expression(status: 'error') }
+  end
+
+  context 'with authorization header' do
+    let!(:env) { authorization_headers }
+
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 200 }
+    its(:body) do
+      is_expected.to match_json_expression(
+        id: params[:id],
+        goal_id: params[:goal_id],
+        date: params[:date].iso8601,
+        value: params[:value]
+      )
+    end
   end
 end
