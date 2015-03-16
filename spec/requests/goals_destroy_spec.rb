c94d850ca1276cc3ac704614f305327ca12727ff
bouzuya <m@bouzuya.net>
2015-03-16 19:58:08 +0900

    fix goals#destroy spec

--- spec/requests/goals_destroy_spec.rb
+++ spec/requests/goals_destroy_spec.rb
@@ -3,13 +3,23 @@ require 'rails_helper'
 RSpec.describe 'goals#destroy', type: :request do
   let!(:model) { create(:goal) }
 
-  let!(:env) { nil }
   let!(:params) { nil }
   let!(:method) { :delete }
   let!(:path) { goal_path(model) }
 
-  before { send(method, path, params, env) }
-  subject { response }
-  its(:status) { is_expected.to be 204 }
-  its(:body) { is_expected.to be_empty }
+  context 'without authorization header' do
+    let!(:env) { nil }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 401 }
+    its(:body) { is_expected.to match_json_expression(status: 'error') }
+  end
+
+  context 'with authorization header' do
+    let!(:env) { authorization_headers }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 204 }
+    its(:body) { is_expected.to be_empty }
+  end
 end
