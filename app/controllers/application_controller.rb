c1d160e8823bb013c8acccd22bb373b8c735aef2
bouzuya <m@bouzuya.net>
2015-03-16 19:31:58 +0900

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_filter :set_format
+  before_filter :authorize
 
   def set_format
     request.format = 'json'
+  end
+
+  def authorize
+    return if authenticate_with_http_token do |token, _options|
+      ENV['SYNCHOKU_TOKEN'] == token
+    end
+    render json: { status: 'error' }, status: :unauthorized
   end
 end
