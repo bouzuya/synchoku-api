415a649868ba76bcf0cc8fc60de940138bcfa2a1
bouzuya <m@bouzuya.net>
2015-03-16 23:00:57 +0900

    fix marks#destroy spec

--- spec/requests/marks_destroy_spec.rb
+++ spec/requests/marks_destroy_spec.rb
@@ -4,13 +4,23 @@ RSpec.describe 'goals#destroy', type: :request do
   let!(:goal) { create(:goal) }
   let!(:model) { create(:mark, goal: goal) }
 
-  let!(:env) { nil }
   let!(:params) { nil }
   let!(:method) { :delete }
   let!(:path) { mark_path(model) }
 
-  before { send(method, path, params, env) }
-  subject { response }
-  its(:status) { is_expected.to be 204 }
-  its(:body) { is_expected.to be_empty }
+  context 'without authorization header' do
+    let!(:env) { nil }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 401 }
+    its(:body) { is_expected.to match_json_expression(status: 'error') }
+  end
+
+  context 'with authorization header' do
+    let!(:env) { authorization_headers }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 204 }
+    its(:body) { is_expected.to be_empty }
+  end
 end
