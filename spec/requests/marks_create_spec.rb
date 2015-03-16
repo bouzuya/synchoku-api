c438ad24c97b6fe61fd89906fb0af7e95e111666
bouzuya <m@bouzuya.net>
2015-03-16 22:31:51 +0900

    fix marks#create spec

--- spec/requests/marks_create_spec.rb
+++ spec/requests/marks_create_spec.rb
@@ -3,20 +3,30 @@ require 'rails_helper'
 RSpec.describe 'marks#create', type: :request do
   let!(:goal) { create(:goal) }
 
-  let!(:env) { nil }
   let!(:params) { attributes_for(:mark) }
   let!(:method) { :post }
   let!(:path) { goal_marks_path(goal) }
 
-  before { send(method, path, params, env) }
-  subject { response }
-  its(:status) { is_expected.to be 201 }
-  its(:body) do
-    is_expected.to match_json_expression(
-      id: Numeric,
-      goal_id: goal.id,
-      date: params[:date].iso8601,
-      value: params[:value]
-    )
+  context 'without authorization header' do
+    let!(:env) { nil }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 401 }
+    its(:body) { is_expected.to match_json_expression(status: 'error') }
+  end
+
+  context 'with authorization header' do
+    let!(:env) { authorization_headers }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 201 }
+    its(:body) do
+      is_expected.to match_json_expression(
+        id: Numeric,
+        goal_id: goal.id,
+        date: params[:date].iso8601,
+        value: params[:value]
+      )
+    end
   end
 end
