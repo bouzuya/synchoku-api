76c3242928ef28317ab25a1f696312a0b0a8c31f
bouzuya <m@bouzuya.net>
2015-03-17 21:07:22 +0900

    hide token in goals#index

--- app/views/goals/index.json.jbuilder
+++ app/views/goals/index.json.jbuilder
@@ -1,4 +1,5 @@
 json.array! @goals do |goal|
-  json.extract! goal, :id, :token, :value, :visible
+  json.extract! goal, :id, :value, :visible
+  json.token goal.token if @token
   json.date goal.date.iso8601
 end
--- spec/requests/goals_index_spec.rb
+++ spec/requests/goals_index_spec.rb
@@ -1,33 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe 'goals#index', type: :request do
-  let!(:env) { nil }
+  let!(:goal1) { create(:goal) }
+  let!(:goal2) { create(:goal) }
+
   let!(:params) { nil }
   let!(:method) { :get }
   let!(:path) { goals_path }
 
-  let!(:goal1) { create(:goal) }
-  let!(:goal2) { create(:goal) }
+  context 'without authorization header' do
+    let!(:env) { nil }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 200 }
+    its(:body) do
+      is_expected.to match_json_expression([
+        {
+          id: goal1.id,
+          date: goal1.date.iso8601,
+          value: goal1.value,
+          visible: goal1.visible
+        },
+        {
+          id: goal2.id,
+          date: goal2.date.iso8601,
+          value: goal2.value,
+          visible: goal2.visible
+        }
+      ])
+    end
+  end
 
-  before { send(method, path, params, env) }
-  subject { response }
-  its(:status) { is_expected.to be 200 }
-  its(:body) do
-    is_expected.to match_json_expression([
-      {
-        id: goal1.id,
-        date: goal1.date.iso8601,
-        token: goal1.token,
-        value: goal1.value,
-        visible: goal1.visible
-      },
-      {
-        id: goal2.id,
-        date: goal2.date.iso8601,
-        token: goal2.token,
-        value: goal2.value,
-        visible: goal2.visible
-      }
-    ])
+  context 'with authorization header' do
+    let!(:env) { authorization_headers }
+    before { send(method, path, params, env) }
+    subject { response }
+    its(:status) { is_expected.to be 200 }
+    its(:body) do
+      is_expected.to match_json_expression([
+        {
+          id: goal1.id,
+          date: goal1.date.iso8601,
+          token: goal1.token,
+          value: goal1.value,
+          visible: goal1.visible
+        },
+        {
+          id: goal2.id,
+          date: goal2.date.iso8601,
+          token: goal2.token,
+          value: goal2.value,
+          visible: goal2.visible
+        }
+      ])
+    end
   end
 end
